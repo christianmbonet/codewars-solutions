34c9d14c72d29e887bfb3e1315b71ad2e7d971b4
christian bonet <christianm.bonet@gmail.com>
2022-04-05 17:14:55 -0400

    adding previous solutions and string ends with solution

---
+++ codewars.js
@@ -0,0 +1,29 @@
+// Multiply 8kyu
+
+function multiply(a, b){
+    return a * b
+  }
+
+// Square every digit 7kyu
+
+function squareDigits(num){
+    let string = num.toString();
+    let results = [];
+    for (i=0; i < string.length; i++){
+      results[i] = string[i] * string[i];
+    }
+    return Number(results.join(''));
+  }
+  
+  squareDigits(2);
+
+// String ends with 7kyu
+
+function solution(str, ending){
+  return str.endsWith(ending);
+}
+
+const solution = () => {
+    
+}
+
