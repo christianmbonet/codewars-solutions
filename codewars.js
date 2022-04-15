bea9b9ca20eee304374dcfae22c8d44540494f88
christian bonet <christianm.bonet@gmail.com>
2022-04-14 22:23:30 -0400

    adding even or odd solution

--- codewars.js
+++ codewars.js
@@ -108,3 +108,13 @@ const reverseSeq = n => {
   }
   return arr
 };
+
+// Even or Odd
+
+function even_or_odd(number) {
+  if (number % 2 === 0){
+    return "Even"
+  } else {
+    return "Odd"
+  }
+}
