7437480592212d3e079a29eae15b7e55d8f2f8c9
christian bonet <christianm.bonet@gmail.com>
2022-05-03 08:25:05 -0400

    update light solution

--- codewars.js
+++ codewars.js
@@ -301,6 +301,17 @@ function sumMix(x){
   }
 }
 
+// Update traffic light
+
+function updateLight(current) {
+  if (current == "green"){
+    return "yellow"
+  } else if (current == "yellow"){
+    return "red"
+  } else if (current == "red"){
+    return "green"
+  }
+}
 
 
 
