1ca9e3157a29ebbadebbd09715185353fc10e6cb
christian bonet <christianm.bonet@gmail.com>
2022-05-07 12:33:39 -0400

    adding solution for greeting language

--- codewars.js
+++ codewars.js
@@ -359,7 +359,34 @@ function opposite(number) {
   return number * -1
 }
 
+// Return greeting that matches language
 
+function greet(language) {
+	const languages = [{
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+}]
+  if (languages[0][language] === undefined){
+    return 'Welcome'
+    } else {
+      return languages[0][language]
+    }
+}
 
 
 
