583171aac69f54356a7276e1436614ea4d7031b1
christian bonet <christianm.bonet@gmail.com>
2022-05-14 20:52:23 -0400

    solution for sum of array

--- codewars.js
+++ codewars.js
@@ -466,3 +466,14 @@ function hero(bullets, dragons){
     return false
   }
  }
+
+ // Sum Arrays
+
+ function sum (numbers) {
+  if (numbers === 0){
+    return 0;
+  } else {
+    return numbers.reduce((a,b) => a + b, 0)
+  }
+};
+
