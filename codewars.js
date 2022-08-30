d25619ad3949b4a86b8ae96442f48ddf1e464451
christian bonet <christianm.bonet@gmail.com>
2022-08-29 22:02:02 -0400

    return likes

--- codewars.js
+++ codewars.js
@@ -567,3 +567,41 @@ function maps(x){
   const map = x.map(i => i*2)
   return map
 }
+
+// Return likes {hard coded} {wrong answer}
+
+function likes(names) {
+  if (names.length === 0){
+    return 'no one likes this'
+  } else if (names.length === 1){
+    return 'Peter likes this'
+  } else if (names.length === 2){
+    return 'Jacob and Alex like this'
+  } else if (names.length === 3){
+    return 'Max, John and Mark like this'
+  } else if (names.length === 4){
+    return 'Alex, Jacob and 2 others like this'
+  } else {
+    let number = names.length - 2
+    return 'Alex, Jacob and ${number} others like this'
+  }
+}
+
+// Return likes {correct}
+
+function likes(names) {
+  if (names.length === 0){
+    return 'no one likes this'
+  } else if (names.length === 1){
+    return `${names[0]} likes this`
+  } else if (names.length === 2){
+    return `${names[0]} and ${names[1]} like this`
+  } else if (names.length === 3){
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`
+  } else if (names.length === 4){
+    return `${names[0]}, ${names[1]} and 2 others like this`
+  } else {
+    let number = names.length - 2
+    return `${names[0]}, ${names[1]} and ${number} others like this`
+  }
+}
