4064893c2dc1d18250fcf02cd15bb03d247f1eb9
christian bonet <christianm.bonet@gmail.com>
2022-09-05 11:00:57 -0400

    needle in a haystack

--- codewars.js
+++ codewars.js
@@ -631,3 +631,9 @@ function areYouPlayingBanjo(name) {
     return `${name} does not play banjo`
   }
 }
+
+// Needle in a haystack
+
+function findNeedle(haystack) {
+  return `found the needle at position ${haystack.indexOf("needle")}`
+}
