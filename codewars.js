9c4f5c8c0bfced8c8cca8854bfcd8f2e7c65d5c4
christian bonet <christianm.bonet@gmail.com>
2022-09-10 22:09:29 -0400

    double character

--- codewars.js
+++ codewars.js
@@ -709,3 +709,12 @@ function check(a, x) {
     }
   }
 }
+
+// Double character
+
+function doubleChar(str) {
+  let string = ''
+  for (let i = 0; i < str.length; i++){
+   string += str[i] + str[i]
+  } return string
+}
