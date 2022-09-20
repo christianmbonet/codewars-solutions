a4978b216865ee3216d90a4744ec9d76b822509a
christian bonet <christianm.bonet@gmail.com>
2022-09-20 19:53:02 -0400

    arithmetic

--- codewars.js
+++ codewars.js
@@ -839,3 +839,17 @@ function twoSum(numbers, target) {
   function getAge(inputString){
     return Number(inputString.charAt(0))
   }
+
+  // Arithmetic 
+
+  function arithmetic(a, b, operator){
+    if (operator == 'add'){
+      return a + b
+    } else if (operator == 'subtract'){
+      return a - b
+    } else if (operator == 'multiply'){
+      return a * b
+    } else if (operator == 'divide'){
+      return a/b
+    }
+  }
