a6dca04e4b29a7c63ea4500efa349805576e8153
christian bonet <christianm.bonet@gmail.com>
2022-10-21 12:46:31 -0400

    sort the odd

--- codewars.js
+++ codewars.js
@@ -1164,3 +1164,34 @@ function roundtonext5(x) {
 function removeChar(str){
   return str.slice(1, -1)
 };
+
+// Sort the odds - wrong solution - attempt - only sorted odd
+
+function sortArray(array) {
+  let newArr = [];
+  for (i=0; i < array.length; i++){
+    if (array[i] % 2 != 0){
+      newArr.push(array[i])
+    }
+  } return newArr.sort((a, b) => a - b)
+}
+
+// Sort the odds - correct solution
+
+function sortArray(array) {
+  const oddArr = [];
+  const evenArr = [];
+  const result = [];for (let i = 0; i < array.length; i += 1) {
+    if (array[i]%2 === 0) {
+      evenArr.push(array[i]);
+    } else {
+      oddArr.push(array[i]);
+    }
+  }  oddArr.sort((a, b) => a - b);  for (let i = 0; i < array.length; i += 1) {
+    if (array[i]%2 === 0) {
+      result.push(evenArr.shift());
+    } else {
+      result.push(oddArr.shift());
+    }
+  }  return result;
+}
