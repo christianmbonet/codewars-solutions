600fca1a81e50c992e42db098c16a556f840b34f
christian bonet <christianm.bonet@gmail.com>
2022-10-24 13:08:49 -0400

    drink about

--- codewars.js
+++ codewars.js
@@ -1195,3 +1195,17 @@ function sortArray(array) {
     }
   }  return result;
 }
+
+// Drink about
+
+function peopleWithAgeDrink(old) {
+  if (old < 14){
+    return 'drink toddy'
+  } else if (old < 18){
+    return 'drink coke'
+  } else if (old < 21){
+    return 'drink beer'
+  } else if (old >= 21){
+    return 'drink whisky'
+  }
+};
