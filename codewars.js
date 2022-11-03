76a06998fdf5d22a9551293fa39733d228199e0b
christian bonet <christianm.bonet@gmail.com>
2022-11-03 14:04:39 -0400

    opposites attract

--- codewars.js
+++ codewars.js
@@ -1280,3 +1280,15 @@ function combat(health, damage) {
     return newHealth
   }
 }
+
+// Opposites attract
+
+function lovefunc(flower1, flower2){
+  if (flower1 % 2 == 0 && flower2 % 2 == 0){
+    return false
+  } else if (flower1 % 2 != 0 && flower2 % 2 != 0){
+    return false
+  } else {
+    return true
+  }
+}
