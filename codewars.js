32ab8a66e861cec9006646da771ac80eefc8af63
christian bonet <christianm.bonet@gmail.com>
2022-11-10 12:26:44 -0500

    feast of beast

--- codewars.js
+++ codewars.js
@@ -1370,3 +1370,13 @@ function mouthSize(animal) {
     return 'wide'
   }
 }
+
+// The feast of many beasts
+
+function feast(beast, dish) {
+  if (beast[0] == dish[0] && beast[beast.length - 1] == dish[dish.length - 1]){
+    return true;
+  } else {
+    return false;
+  }
+ }
