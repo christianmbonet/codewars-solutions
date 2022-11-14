43b1c4feefb2ba056f17a25fb5beb032cbab3aba
christian bonet <christianm.bonet@gmail.com>
2022-11-14 12:47:03 -0500

    hello world

--- codewars.js
+++ codewars.js
@@ -1380,3 +1380,9 @@ function feast(beast, dish) {
     return false;
   }
  }
+
+ // Hello Name or World
+
+ function hello(name) {
+  return `Hello, ${name ? name[0].toUpperCase()  + name.slice(1).toLowerCase() : "World"}!`;
+}
