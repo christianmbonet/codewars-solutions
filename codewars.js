b5a881c3a4eecd314eb63f0423d0946d263ae9c9
christian bonet <christianm.bonet@gmail.com>
2022-12-09 12:38:50 -0500

    two sum

--- codewars.js
+++ codewars.js
@@ -1536,3 +1536,14 @@ function accum(s) {
 
   return ans.join("-")
 }
+
+// Two sum
+
+function twoSum(numbers, target) {
+  for(let i = 0; i<numbers.length; i++){
+    for(let j = i+1; j<numbers.length; j++){
+      let res = numbers[i] + numbers[j]
+      if(res == target) return [i,j]
+    }
+  }
+}
