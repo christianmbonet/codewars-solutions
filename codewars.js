b75ff76ef0f9b26bf359c2e172c7b2299ec85244
christian bonet <christianm.bonet@gmail.com>
2022-12-13 12:57:46 -0500

    how many lightsabers

--- codewars.js
+++ codewars.js
@@ -1557,3 +1557,13 @@ function findUniq(arr) {
    );
  }
  
+
+// How many light sabers do you own
+
+function howManyLightsabersDoYouOwn(name) {
+  if (name === 'Zach'){
+    return 18
+  } else {
+    return 0
+  }
+}
