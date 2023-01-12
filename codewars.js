da5008b84006ac2f0bceec2d30e5f56b840ce5a5
christian bonet <christianm.bonet@gmail.com>
2023-01-12 12:43:25 -0500

    sum of mixed array

--- codewars.js
+++ codewars.js
@@ -1683,3 +1683,16 @@ function helloWorld(){
   const str = 'Hello World!'
   console.log(str)
   }
+
+// Sum of mixed data types
+
+function sumMix(x){
+  let sum = 0;
+  for(let i = 0; i < x.length; i++) {
+  if (typeof x[i] === 'string') {
+    sum += Number(x[i])
+  } else {
+    sum += x[i]
+  } 
+} return sum
+}
