f03bf55d2d03419b2333a3de00cd8e524fc1f85a
christian bonet <christianm.bonet@gmail.com>
2023-01-13 12:57:14 -0500

    alternating case

--- codewars.js
+++ codewars.js
@@ -1696,3 +1696,17 @@ function sumMix(x){
   } 
 } return sum
 }
+
+//altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+
+String.prototype.toAlternatingCase = function () {
+  let str = '';
+  for(let i = 0; i< this.length; i++){
+    if (this[i] == this[i].toLowerCase()){ 
+      str += this[i].toUpperCase();
+    } else {
+      str += this[i].toLowerCase();
+    }
+  }
+  return str
+}
