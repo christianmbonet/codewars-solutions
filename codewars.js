faa529727061cff27027c0201d054a944c0f5a38
christian bonet <christianm.bonet@gmail.com>
2023-03-02 13:28:36 -0500

    check exam

--- codewars.js
+++ codewars.js
@@ -2021,3 +2021,21 @@ function dutyFree(normPrice, discount, hol){
   return Math.floor(hol / normPrice / discount * 100)
  }
 
+ // Check the exam
+
+ function checkExam(array1, array2) {
+  let score = 0
+   for (i=0; i < array1.length; i++){
+     if (array1[i] === array2[i]){
+       score = score + 4
+     } else if (array2[i] === ""){
+       score = score - 0
+     }
+     else if (array1[i] !== array2[i]){
+       score = score - 1
+     }
+   } if (score < 0){
+     score = 0
+   } return score
+ }
+
