a644addb3b87ac1f16eb594c6ed09a014f22fbdd
christian bonet <christianm.bonet@gmail.com>
2023-03-16 12:33:54 -0400

    sum mixed arrays

--- codewars.js
+++ codewars.js
@@ -2153,3 +2153,15 @@ function answer3(){
   return "yes";
 }
 
+// Sum mixed arrays - duplicate
+
+function sumMix(x){
+  let sum = 0;
+  for(let i = 0; i < x.length; i++) {
+  if (typeof x[i] === 'string') {
+    sum += Number(x[i])
+  } else {
+    sum += x[i]
+  } 
+} return sum
+}
