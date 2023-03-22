9788368330a47ce9ae0af4576ccfd437af5d7f51
christian bonet <christianm.bonet@gmail.com>
2023-03-22 12:37:36 -0400

    find the odd int

--- codewars.js
+++ codewars.js
@@ -2189,3 +2189,20 @@ function cockroachSpeed(s) {
 var uniqueInOrder=function(iterable){
   return [...iterable].filter((element, index) => element != iterable[index + 1])
 }
+
+// Find the odd int
+
+function findOdd(A) {
+  let count = 0;
+  let arr = A.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] == arr[j]) {
+        count++;
+      }
+    }
+    if (count % 2 !== 0) {
+      return arr[i];
+    }
+  }
+}
