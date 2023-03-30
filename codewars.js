1c74c57be8e021ad3bce9d20c8e6556b92d157a8
christian bonet <christianm.bonet@gmail.com>
2023-03-30 13:05:57 -0400

    odd or even

--- codewars.js
+++ codewars.js
@@ -2240,3 +2240,9 @@ function position(letter){
   let alpha = "0abcdefghijklmnopqrstuvwxyz"
    return `Position of alphabet: ${alpha.indexOf(letter)}`
  }
+
+ // Repeat - Odd or Even
+
+ function oddOrEven(array) {
+  return array.reduce((a, b) => a + b, 0) % 2 ? "odd" : "even"
+ }
