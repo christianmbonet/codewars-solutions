12a32529d3dd17c5919a7b50ec2df68039d5db33
christian bonet <christianm.bonet@gmail.com>
2023-04-04 12:41:01 -0400

    switch case

--- codewars.js
+++ codewars.js
@@ -2264,3 +2264,48 @@ function getSize(width, height, depth){
   return [(2 * width * height) + (2 * height * depth) + (2 * depth * width), width * height * depth]
 }
 
+// Training JS #8: Conditional statement--switch
+
+function howManydays(month){
+  var days;
+  switch (month){
+    case 1: 
+      days=31
+      break;
+    case 2:
+      days=28
+      break;
+    case 3:
+      days=31
+      break;
+    case 4:
+      days=30
+      break;
+    case 5:
+      days=31
+      break;
+    case 6:
+      days=30
+      break;
+    case 7:
+      days=31
+      break;
+    case 8:
+      days=31
+      break;
+    case 9:
+      days=30
+      break;
+    case 10:
+      days=31
+      break;
+    case 11:
+      days=30
+      break;
+    case 12:
+      days=31
+      break;
+  }
+  return days;
+}
+
