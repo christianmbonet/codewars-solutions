1bf80e3a0c96cb9b24ae5e0fad787355369fedb7
christian bonet <christianm.bonet@gmail.com>
2023-05-03 12:45:16 -0400

    fundamentals return

--- codewars.js
+++ codewars.js
@@ -2455,3 +2455,29 @@ function roundToNext5(n){
 function minValue(values){
   return +[...new Set(values)].sort().join('')
 }
+
+// Fundamentals return
+
+function add(a,b){
+  return a + b
+}
+
+function divide(a,b){
+  return a/b
+}
+
+function multiply(a,b){
+  return a * b
+}
+
+function mod(a,b){
+  return a % b
+}
+ 
+function exponent(a,b){
+  return a ** b
+}
+  
+function subt(a,b){
+  return a - b
+}
