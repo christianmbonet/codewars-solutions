94decb98e31494197ce8a0174d36ee92056b8024
christian bonet <christianm.bonet@gmail.com>
2023-05-15 12:30:44 -0400

    greet me

--- codewars.js
+++ codewars.js
@@ -2532,3 +2532,9 @@ function evenNumbers(array, number) {
     }
   } return arr.slice(-number)
 }
+
+// Greet me 
+
+var greet = function(name) {
+  return `Hello ${(name.slice(0,1).toUpperCase())+(name.slice(1).toLowerCase())}!`
+};
