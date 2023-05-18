38e0c16f2752739660780e0b88bd2d8ef5a276c7
christian bonet <christianm.bonet@gmail.com>
2023-05-18 12:34:30 -0400

    mixed array sum

--- codewars.js
+++ codewars.js
@@ -2566,3 +2566,16 @@ function pickIt(arr){
   
   return [odd,even];
 }
+
+// Sum mixed array - repeat
+
+function sumMix(x){
+  let sum = 0;
+  for(let i = 0; i < x.length; i++) {
+  if (typeof x[i] === 'string') {
+    sum += Number(x[i])
+  } else {
+    sum += x[i]
+  } 
+} return sum
+}
