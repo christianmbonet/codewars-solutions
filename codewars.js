61aea2fc3973a94e0ee236b841dbf6746b134042
christian bonet <christianm.bonet@gmail.com>
2023-05-31 12:34:38 -0400

    combine

--- codewars.js
+++ codewars.js
@@ -2621,3 +2621,9 @@ function isSortedAndHow(array) {
   return array.every((x,i)=>i==0||array[i]>=array[i-1])?'yes, ascending':
          array.every((x,i)=>i==0||array[i]<=array[i-1])?'yes, descending':'no'
 }
+
+// Combine
+
+const combineNames = (first, lastName) => {
+  return `${first} ${lastName}`
+  }
