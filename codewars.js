3d03174793cf711490dc17755c0a9ad181fcf4c8
christian bonet <christianm.bonet@gmail.com>
2023-06-05 12:42:14 -0400

    string case change

--- codewars.js
+++ codewars.js
@@ -2644,3 +2644,11 @@ function remove(s,n){
   }
   return input;
 }
+
+// Fix string case
+
+function solve(s){
+  let up=s.split('').filter(v=>v.match(/[A-Z]/)).length
+  let down=s.split('').filter(v=>v.match(/[a-z]/)).length
+  return down>=up?s.toLowerCase():s.toUpperCase();
+}
