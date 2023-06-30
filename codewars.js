d6fc9290a819e14a4ee7823417bdb04b2786a0e2
christian bonet <christianm.bonet@gmail.com>
2023-06-30 13:17:58 -0400

    isogram

--- codewars.js
+++ codewars.js
@@ -2777,3 +2777,18 @@ function remainder(n, m){
   }
 }
 
+// Isograms - not fully understood answer - how i to j?
+
+function isIsogram(str){
+  var i, j;
+  str = str.toLowerCase();
+  for(i = 0; i < str.length; ++i) {
+    for(j = i + 1; j < str.length; ++j) {
+      if(str[i] === str[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
