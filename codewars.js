de2da29f15306b22d2cdbfcfe024f6deb0f36c99
christian bonet <christianm.bonet@gmail.com>
2023-07-05 12:42:56 -0400

    sleigh authenticator

--- codewars.js
+++ codewars.js
@@ -2792,3 +2792,15 @@ function isIsogram(str){
   return true;
 }
 
+// Sleigh authenticator
+
+function Sleigh() {}
+
+Sleigh.prototype.authenticate = function(name, password) {
+  if (name === 'Santa Claus' && password === 'Ho Ho Ho!'){
+    return true
+  } else {
+    return false
+  }
+};
+
