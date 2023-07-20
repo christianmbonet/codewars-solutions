6350cb7a9d886ec79158523ea244426b1fe40184
christian bonet <christianm.bonet@gmail.com>
2023-07-20 12:56:12 -0400

    rating and tip

--- codewars.js
+++ codewars.js
@@ -2886,3 +2886,27 @@ function abbrevName(name){
   .map((thing) => thing[0].toUpperCase())
   .join('.')
 }
+
+// Rating and tip
+
+function calculateTip(amount, rating) {
+  let rate = rating.toLowerCase();
+    if(rate === "excellent"){
+    return Math.ceil(amount*20/100);
+  }
+    else if(rate === "great"){
+    return Math.ceil(amount*15/100);
+  }
+    else if(rate === "good"){
+    return Math.ceil(amount*10/100);
+  }
+    else if(rate === "poor"){
+    return Math.ceil(amount*5/100);
+  }
+    else if(rate === "terrible"){
+    return Math.ceil(amount*0/100);
+  }
+    else{
+  return("Rating not recognised");
+  } 
+  }
