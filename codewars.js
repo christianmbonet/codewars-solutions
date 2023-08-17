5d006173ba367697649751bfc7815b32384b3796
christian bonet <christianm.bonet@gmail.com>
2023-08-17 12:42:28 -0400

    leo di and osc

--- codewars.js
+++ codewars.js
@@ -3068,3 +3068,12 @@ function countRedBeads(n) {
     return n * 2 - 2
   }
 }
+
+// Leo Di and OSC
+
+function leo(oscar){
+  if (oscar === 88) return ('Leo finally won the oscar! Leo is happy');
+  if (oscar === 86) return ('Not even for Wolf of wallstreet?!');
+  if (oscar < 88) return ('When will you give Leo an Oscar?');
+  if (oscar > 88) return ('Leo got one already!');
+  }
