0aa1ad52035dd08152385deaeb93f183deb3332b
christian bonet <christianm.bonet@gmail.com>
2023-09-29 12:32:24 -0400

    minimum sum array

--- codewars.js
+++ codewars.js
@@ -3294,3 +3294,14 @@ function  calculateAge(yob, year) {
 function toCsvText(array) {
   return array.join('\n');
 }
+
+// Minimum sum array
+
+function minSum(arr) {
+  const sorted=arr.sort((a,b)=>a-b)
+  let sum=0;
+    for (let i=0;i<arr.length/2;i++){
+      sum+=sorted[i]*sorted[sorted.length-1-i]
+    }
+  return sum
+}
