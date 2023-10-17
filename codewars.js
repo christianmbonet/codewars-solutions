9f2bcc5b9dda4814936e0c82e9f6c2abc6d61c41
christian bonet <christianm.bonet@gmail.com>
2023-10-17 12:42:20 -0400

    regex dash

--- codewars.js
+++ codewars.js
@@ -3399,3 +3399,13 @@ function leo(oscar){
  String.prototype.digit = function() {
   return /^\d$/.test(this);
 };
+
+// Dashatize it - regex practice
+
+function dashatize(num) {
+  return num.toString().
+    replace(/([13579])/g,'-$1-'). // dashes around odd digits
+    replace(/\-+/g,'-'). // replace multiple dashes by one dash
+    replace(/^\-/,''). // remove starting dash
+    replace(/\-$/,''); // remove ending dash
+}
