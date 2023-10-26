66f7f31695ba3fd75e6118ceb6953b12949005e1
christian bonet <christianm.bonet@gmail.com>
2023-10-26 12:38:22 -0400

    data reverse

--- codewars.js
+++ codewars.js
@@ -3457,3 +3457,12 @@ function plus( b )      { return function( a ) { return a + b; }; };
 function minus( b )     { return function( a ) { return a - b; }; };
 function times( b )     { return function( a ) { return a * b; }; };
 function dividedBy( b ) { return function( a ) { return Math.floor(a/b); }; };
+
+// Data reverse - repeat
+
+function dataReverse(data) {
+  let final = []
+  for (i=0;i<data.length;i++){
+    final.push(data[i])
+  } return final
+}
