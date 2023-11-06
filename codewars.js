063d62af1ade7633b462e6eceea1642552960e25
christian bonet <christianm.bonet@gmail.com>
2023-11-06 12:27:14 -0500

    two fighters

--- codewars.js
+++ codewars.js
@@ -3502,10 +3502,18 @@ function validateCode (code) {
 
  // Will you make it - repeat
 
- const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+ const Fuel = (distanceToPump, mpg, fuelLeft) => {
   if (distanceToPump <= (fuelLeft * mpg)){
     return true
   } else {
     return false
   }
 };
+
+// Two fighters one winner
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+  let f1 = Math.ceil(fighter1.health / fighter2.damagePerAttack)
+  let f2 = Math.ceil(fighter2.health / fighter1.damagePerAttack)
+  return f1 < f2 ? fighter2.name : f2 < f1 ? fighter1.name : firstAttacker
+}
