3347ec6a1848c5504bfce58452daa5b5e44f9323
christian bonet <christianm.bonet@gmail.com>
2023-11-07 14:04:06 -0500

    parse float

--- codewars.js
+++ codewars.js
@@ -3517,3 +3517,9 @@ function declareWinner(fighter1, fighter2, firstAttacker) {
   let f2 = Math.ceil(fighter2.health / fighter1.damagePerAttack)
   return f1 < f2 ? fighter2.name : f2 < f1 ? fighter1.name : firstAttacker
 }
+
+// Parse float 
+
+function parseF(s) {
+  return /\d/.test(s) ? +s : null
+}
