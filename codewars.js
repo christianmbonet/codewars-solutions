ad75909a1fb4fb2b12f843c8c23f3a3f2f1a5a1a
christian bonet <christianm.bonet@gmail.com>
2023-11-21 14:31:14 -0500

    object oriented piracy

--- codewars.js
+++ codewars.js
@@ -3581,3 +3581,18 @@ function solve(arr) {
   }
   return arr
 }
+
+// OOP Object oriented piracy
+
+function Ship(draft,crew) {
+  this.draft = draft;
+  this.crew = crew;
+ }
+ 
+ Ship.prototype.isWorthIt = function () {
+   const CREW_MEMBER_WEIGHT = 1.5
+   const MINIMUM_BOOTY = 20
+ 
+   return this.draft - this.crew * CREW_MEMBER_WEIGHT > MINIMUM_BOOTY
+ }
+ 
