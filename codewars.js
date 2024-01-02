68360267ac5ead796d422882ed4030a53bf881ff
christian bonet <christianm.bonet@gmail.com>
2024-01-02 12:40:43 -0500

    check exam

--- codewars.js
+++ codewars.js
@@ -3735,3 +3735,21 @@ function solution(string) {
       }
     } return newString
 }
+
+// Check the exam - repeat
+
+function checkExam(array1, array2) {
+  let score = 0
+   for (i=0; i < array1.length; i++){
+     if (array1[i] === array2[i]){
+       score = score + 4
+     } else if (array2[i] === ""){
+       score = score - 0
+     }
+     else if (array1[i] !== array2[i]){
+       score = score - 1
+     }
+   } if (score < 0){
+     score = 0
+   } return score
+ }
