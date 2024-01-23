fe6daa511d7b3f86648abd224bbf550548a9efaf
christian bonet <christianm.bonet@gmail.com>
2024-01-23 12:30:18 -0500

    data reverse

--- codewars.js
+++ codewars.js
@@ -3845,3 +3845,15 @@ function warnTheSheep(queue) {
 function spacify(str) {
   return str.split('').join(' ')
 }
+
+// Data reverse
+
+const dataReverse = function(data){
+  if (data.length===0) return []
+  const arrayBytes = [];
+  const tam = data.length / 8;
+  for (var i = 0; i < tam; i++){
+    arrayBytes.push(data.splice(0,8));
+  }
+  return arrayBytes.reverse().join().split(',').map(v=>v*1);
+}
