90065bea10e2b8401911c166abd4424b0b619220
christian bonet <christianm.bonet@gmail.com>
2024-02-02 12:28:15 -0500

    supermarket

--- codewars.js
+++ codewars.js
@@ -3884,3 +3884,15 @@ function func (str) {
   let arr = str.toLowerCase().split('');
   return arr.filter(element => element == 'x').length === arr.filter(element => element == 'o').length
 }
+
+// Supermarket queue
+
+function queueTime(customers, n) {
+  var arr = new Array(n).fill(0);
+  for (var i = 0; i < customers.length; i++) {
+    var idx = arr.indexOf(Math.min(...arr));
+    arr[idx] += customers[i];
+  }
+
+  return Math.max(...arr);
+}
