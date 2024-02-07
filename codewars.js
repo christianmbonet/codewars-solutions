4f26be8fdaf32a9c857182e43d3bf2a6a4c842ae
christian bonet <christianm.bonet@gmail.com>
2024-02-07 12:35:42 -0500

    minimize sum of array

--- codewars.js
+++ codewars.js
@@ -3916,3 +3916,14 @@ function sameCase(a, b){
     return 0
   }
 }
+
+// Minimize
+
+function minSum(arr) {
+  const sorted=arr.sort((a,b)=>a-b)
+ let sum=0;
+ for (let i=0;i<arr.length/2;i++){
+ sum+=sorted[i]*sorted[sorted.length-1-i]
+ }
+ return sum
+}
