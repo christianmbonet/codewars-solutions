1866c5acc82d678c2cb9bd19824a7fd6ef14bd62
christian bonet <christianm.bonet@gmail.com>
2024-02-15 13:14:30 -0500

    sum of odd cubed

--- codewars.js
+++ codewars.js
@@ -3927,3 +3927,18 @@ function minSum(arr) {
  }
  return sum
 }
+
+// Sum of odd cubed numbers
+
+function cubeOdd(arr) {
+  let odd = [];
+    let cubed = arr.map((el) => Math.pow(el, 3));
+    if (arr.filter((element) => typeof(element) == 'number').length == arr.length) {
+      for (let i=0; i<cubed.length; i++) {
+        if (cubed[i] % 2 != 0) odd.push(cubed[i]);
+      }
+      return odd.reduce((a,b) => a+b, 0);
+    } else {
+        return undefined;
+      }
+  }
