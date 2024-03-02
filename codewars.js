59ab440a5dbdea9e2201d24083f49a4188cae9e2
christian bonet <christianm.bonet@gmail.com>
2024-03-02 10:06:56 -0500

    growth of a population

--- codewars.js
+++ codewars.js
@@ -4022,3 +4022,11 @@ function cubeOdd(arr) {
 function findLongest(array){
   return array.reduce((a, b) => (`${b}`.length > `${a}`.length ? b : a))
 }
+
+// Growth of a population
+
+function nbYear(p0, percent, aug, p) {
+  let year=0;
+   while (p0<p){year++; p0=p0+(p0*(percent/100))+aug}
+    return year
+ }
