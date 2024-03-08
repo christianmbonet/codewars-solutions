6d8597fcb51480593ae872c20d5b80e57c002f11
christian bonet <christianm.bonet@gmail.com>
2024-03-08 12:42:43 -0500

    digit explosion

--- codewars.js
+++ codewars.js
@@ -4083,3 +4083,15 @@ function sumMul(n,m){
      sum += i
    } return sum
  }
+
+ // Digit explosion
+
+ function explode(s) {
+  return s.split('').map(d=>{
+    let y = '';
+    for(let i = 0; i < +d; i++){
+     y += d
+    }
+    return y
+  }).join('')
+}
