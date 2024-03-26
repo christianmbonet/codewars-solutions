b6a06b013dcbb6ab3d9027c53a1dd6ce3becedce
christian bonet <christianm.bonet@gmail.com>
2024-03-26 13:50:08 -0400

    functional addition

--- codewars.js
+++ codewars.js
@@ -4176,3 +4176,11 @@ const comp = (a1, a2) => {
   if (!a1 || !a2 || a1.length !== a2.length) return false;
   return a1.map(x => x * x).sort().toString() === a2.sort().toString();
 }
+
+// Functional addition
+
+function add(n) {
+  return function (m) {
+    return n+m;
+    }
+}
