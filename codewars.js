022a905d9c8dc2a4df83361314ef615049cd76b5
christian bonet <christianm.bonet@gmail.com>
2024-04-01 12:28:20 -0400

    break continue

--- codewars.js
+++ codewars.js
@@ -4203,3 +4203,17 @@ function capitalize(s,arr){
 //if it equals one of arr
 //toUppercase
 //forgot the split
+
+// Loop statement break continue
+
+function grabDoll(dolls){
+  let bag=[];
+    for(let i = 0; i < dolls.length; i++) {
+      if(dolls[i] === 'Hello Kitty' || dolls[i] === 'Barbie doll') { 
+        bag.push(dolls[i])  
+      }
+        if(bag.length === 3) {break;}
+      else {continue}
+    }
+    return bag;
+}
