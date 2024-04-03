ace957f55debc3b8e591547aed67b52be1dc0b35
christian bonet <christianm.bonet@gmail.com>
2024-04-03 12:40:07 -0400

    all inclusive

--- codewars.js
+++ codewars.js
@@ -4238,3 +4238,12 @@ var encryptThis = function(text) {
     })
     .join(' ');
 }
+
+// All inclusive 
+
+for (var i = 0; i < str.length; i++) {
+  if (arr.indexOf(str.slice(i) + str.slice(0, i)) === -1) {
+    return false
+  }
+}
+return true
