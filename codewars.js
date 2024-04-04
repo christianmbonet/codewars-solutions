2ce463509b1ee655ae9de24648acddd6e3653132
christian bonet <christianm.bonet@gmail.com>
2024-04-04 12:37:09 -0400

    two sum

--- codewars.js
+++ codewars.js
@@ -4247,3 +4247,14 @@ for (var i = 0; i < str.length; i++) {
   }
 }
 return true
+
+// Two sum
+
+function twoSum(numbers, target) {
+  for(let i = 0; i<numbers.length; i++){
+    for(let j = i+1; j<numbers.length; j++){
+      let res = numbers[i] + numbers[j]
+      if(res == target) return [i,j]
+    }
+  }
+  } 
