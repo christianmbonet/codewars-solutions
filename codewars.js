620c64d94ebb36290afed39fced936c15f85dc84
christian bonet <christianm.bonet@gmail.com>
2024-04-15 12:22:15 -0400

    string incrementer

--- codewars.js
+++ codewars.js
@@ -4285,3 +4285,13 @@ function periodIsLate(last, today, cycleLength) {
 }
 
 // Editing my sister's writing today -- no codewars
+
+// String incrementer
+
+function incrementString (strng) {
+  if(isNaN(parseInt(strng[strng.length - 1]))) return strng + '1';
+  return strng.replace(/(0*)([0-9]+$)/, function(match, p1, p2) {
+    var up = parseInt(p2) + 1;
+    return up.toString().length > p2.length ? p1.slice(0, -1) + up : p1 + up;
+  });
+}
