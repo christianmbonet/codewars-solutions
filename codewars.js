2dba37f2f7c3477adef659158c211ffc9eeedd88
christian bonet <christianm.bonet@gmail.com>
2024-07-19 12:43:41 -0400

    the vowel code

--- codewars.js
+++ codewars.js
@@ -4615,3 +4615,11 @@ function duplicateEncode(word){
 }).join('')
 }
 
+// The vowel code 
+
+const vowels = ['a', 'e' ,'i', 'o', 'u'];
+const v4w2ls = ['1', '2', '3', '4', '5']; 
+
+const encode = str => [...str].map(e => vowels.includes(e) ? e = vowels.indexOf(e) + 1 : e).join('');
+const decode = str => [...str].map(e => v4w2ls.includes(e) ? e = vowels[v4w2ls.indexOf(e)] : e).join('');
+
