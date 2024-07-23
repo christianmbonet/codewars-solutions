960e7f51f232815ce4e893a5c8f71bd12966c1ee
christian bonet <christianm.bonet@gmail.com>
2024-07-23 12:34:38 -0400

    all inclusive

--- codewars.js
+++ codewars.js
@@ -4638,3 +4638,14 @@ function grader(score) {
     return "D"
   }
 }
+
+// All inclusive
+
+function containAllRots(str, arr) {
+  for (var i = 0; i < str.length; i++) {
+  if (arr.indexOf(str.slice(i) + str.slice(0, i)) === -1) {
+    return false
+  }
+}
+return true
+}
