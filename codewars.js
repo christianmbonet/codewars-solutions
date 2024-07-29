9403eb64a0e265bbc2b525e52cc907d539913116
christian bonet <christianm.bonet@gmail.com>
2024-07-29 12:23:14 -0400

    array helpers

--- codewars.js
+++ codewars.js
@@ -4662,3 +4662,12 @@ function findLongest(array){
   return array.reduce((a, b) => (`${b}`.length > `${a}`.length ? b : a))
 }
 
+// Array helpers
+
+Array.prototype.square  = function () { return this.map(function(n) { return n*n; }); }
+Array.prototype.cube    = function () { return this.map(function(n) { return n*n*n; }); }
+Array.prototype.average = function () { return this.sum() / this.length; }
+Array.prototype.sum     = function () { return this.reduce(function(a, b) { return a + b; }, 0); }
+Array.prototype.even    = function () { return this.filter(function(item) { return 0 == item % 2; }); }
+Array.prototype.odd     = function () { return this.filter(function(item) { return 0 != item % 2; }); }
+
