36ab383b88a6dc758d8567beb01e26fe9897d01e
christian bonet <christianm.bonet@gmail.com>
2024-08-08 12:45:56 -0400

    simple string characters

--- codewars.js
+++ codewars.js
@@ -4729,3 +4729,13 @@ function multiplyAll(arr) {
   }
 }
 
+// Simple string characters
+
+function solve(s){
+  let upper = (s.match(/[A-Z]/g) || []).length,
+      lower = (s.match(/[a-z]/g) || []).length,
+      number = (s.match(/[0-9]/g) || []).length,
+      special = s.length - upper - lower - number
+  return [upper, lower, number, special]
+ }
+
