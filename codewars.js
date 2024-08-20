4cf7b88a1477508e9bf8591fefe7775b2f6113a8
christian bonet <christianm.bonet@gmail.com>
2024-08-20 12:31:41 -0400

    basic calculator

--- codewars.js
+++ codewars.js
@@ -4802,3 +4802,13 @@ function quadrant(x, y) {
     return 3
   }
 }
+
+// Basic calculator
+
+function calculate(num1, operation, num2) {
+  if(operation === "+") return num1 + num2;
+     if(operation === "-") return num1 - num2;
+     if(operation === "/") return num2 !== 0 ? num1/num2 : null;
+     if(operation === '*') return num1 * num2;
+     return null
+ }
