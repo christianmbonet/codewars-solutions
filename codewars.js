7d8d9f61a6b7c713a68c4a5ccb49736ab0e039f5
christian bonet <christianm.bonet@gmail.com>
2024-08-30 12:41:31 -0400

    days in the year

--- codewars.js
+++ codewars.js
@@ -4880,3 +4880,17 @@ function stringTransformer(str) {
     })
     .join("");
 }
+
+// Days in the year
+
+function yearDays(year) {
+  if (year % 4 !== 0) {
+    return year + ' has 365 days';
+  } else if (year % 100 !== 0) {
+    return year + ' has 366 days';
+  } else if (year % 400 !== 0) {
+    return year + ' has 365 days';
+  } else {
+    return year + ' has 366 days';
+  }
+}
