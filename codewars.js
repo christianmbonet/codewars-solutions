8b78a62c41f8f8573e5a2b152c38f80b64fdc48e
christian bonet <christianm.bonet@gmail.com>
2024-09-05 12:42:24 -0400

    building blocks repeat

--- codewars.js
+++ codewars.js
@@ -4905,3 +4905,22 @@ function jumpingNumber(n){
     }
     } return 'Jumping!!'
 }
+
+// Building Blocks - repeat
+
+class Block {
+  constructor(data){
+    [this.w, this.l, this.h] = data;
+  }
+  getWidth(){ return this.w }
+  getLength(){ return this.l }
+  getHeight(){ return this.h }
+  getVolume(){
+    let {l, w, h} = this;
+    return w*l*h;
+  }
+  getSurfaceArea(){
+    let {l, w, h} = this;
+    return 2 * (l*w + l*h + w*h);
+  }
+}
