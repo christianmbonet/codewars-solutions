a3099d9768839149d1e7347b7add8094de0a7c7b
christian bonet <christianm.bonet@gmail.com>
2024-09-16 12:42:13 -0400

    delete if

--- codewars.js
+++ codewars.js
@@ -4956,3 +4956,13 @@ const sequenceSum = (begin, end, step) => {
   }
   return result
   }
+
+// Delete occurrence if it occurs more than n times
+
+function deleteNth(arr,x) {
+  var cache = {};
+  return arr.filter(function(n) {
+    cache[n] = (cache[n]||0) + 1;
+    return cache[n] <= x;
+  });
+}
