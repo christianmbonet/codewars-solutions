8839ff74832e004c3270f451488937dfb93802cd
christian bonet <christianm.bonet@gmail.com>
2024-10-07 12:21:06 -0400

    multiplication table

--- codewars.js
+++ codewars.js
@@ -5042,3 +5042,18 @@ function catMouse(x){
 function catMouse(x){
   return /\.\.\.\./.test(x)?"Escaped!":"Caught!"
   }
+
+// Multiplication table
+
+multiplicationTable = function(size) {
+  let r = []
+  for(let i = 1; i<=size; i++){
+    let x = []
+    for(let j = 1; j<=size; j++){
+      x.push(i*j)
+    }
+    r.push(x)
+  }
+  return r
+}
+
