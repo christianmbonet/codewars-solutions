80e0eea90b5acbd8e8d306ce9c2d4620293334b3
christian bonet <christianm.bonet@gmail.com>
2024-10-15 13:07:28 -0400

    consecutive strings

--- codewars.js
+++ codewars.js
@@ -5102,3 +5102,22 @@ function partlist(arr) {
 function partlist(arr) {
   return arr.map((_,index) => [arr.slice(0,index).join(' '), arr.slice(index).join(' ')]).slice(1)
   }
+
+// Longest consecutive string
+
+function longestConsec(strarr, k) {
+  let arrLength = strarr.length;
+  let arr = [];
+  
+  if(arrLength == 0 || k > arrLength || k <= 0) return "";
+    
+  for(let i = 0; i <= arrLength - k; i++){
+    let current = strarr[i];
+    for(let ii = k, jj = 1; ii > 1; ii--, jj++){
+      current += strarr[i+jj]
+    }
+  arr.push(current);
+  }
+ return arr.reduce(function (a, b) {
+        return a.length > b.length ? a : a.length == b.length ? a : b;
+    })}
