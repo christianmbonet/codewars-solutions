259ae30eb81fff031696626c260bb26a6d58808c
christian bonet <christianm.bonet@gmail.com>
2024-10-29 12:40:17 -0400

    check three and two

--- codewars.js
+++ codewars.js
@@ -5237,3 +5237,12 @@ function countSmileys(arr) {
   }
   return count;
 }
+
+// Check three and two
+
+function checkThreeAndTwo(array) {
+  let a=array.filter(value=>value.match('a')).length;
+  let b=array.filter(value=>value.match('b')).length
+  let c=array.filter(value=>value.match('c')).length
+    return ((a===3||b===3||c===3)&&(a===2||b===2||c===2))
+  }
