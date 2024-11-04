23f12f2d26a742e166d8392fe7c286e4bd926d9d
christian bonet <christianm.bonet@gmail.com>
2024-11-04 12:34:28 -0500

    folding your way to the moon

--- codewars.js
+++ codewars.js
@@ -5261,3 +5261,12 @@ function wave(str){
  }
  return waveArr;
 }
+
+// Folding your way to the moon - repeat
+
+function foldTo(distance) {
+  if (distance >= 0.0001) {
+    return Math.ceil(Math.log2(distance * 10000));
+  }
+  return distance > 0 ? 0 : null;
+}
