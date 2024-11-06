30519585a9ccd0d66e3c1609e60ea9c8c696101b
christian bonet <christianm.bonet@gmail.com>
2024-11-06 12:46:34 -0500

    fuel calculator

--- codewars.js
+++ codewars.js
@@ -5290,3 +5290,14 @@ function charValue(c){
     return 0
   } return c.charCodeAt() - 96
 }
+
+// Fuel calculator
+
+function fuelPrice(litres, pricePerLiter) {
+  if(litres > 9) return Number((litres * (pricePerLiter - 0.25)).toFixed(2));
+  if(litres > 7) return Number((litres * (pricePerLiter - 0.20)).toFixed(2));
+  if(litres > 5) return Number((litres * (pricePerLiter - 0.15)).toFixed(2));
+  if(litres > 3) return Number((litres * (pricePerLiter - 0.10)).toFixed(2));
+  if(litres > 1) return Number((litres * (pricePerLiter - 0.5)).toFixed(2));
+  return litres * pricePerLiter;
+}
