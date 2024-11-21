eee3f15d820c9ff1c055bb3387668c222a5897d6
christian bonet <christianm.bonet@gmail.com>
2024-11-21 12:37:56 -0500

    last survivor

--- codewars.js
+++ codewars.js
@@ -5336,3 +5336,13 @@ function solution(value){
 var filterString = function(value) {
   return parseInt(value.replace(/[a-z]/gi,''))
 }
+
+// Last survivor 
+
+function lastSurvivor(letters, coords) {
+  letters = letters.split('');
+        for (let i = 0; i < coords.length; i++) {
+            letters.splice(coords[i], 1)
+        }
+        return letters.join('')
+}
